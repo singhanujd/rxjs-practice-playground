9985756029c2d6c4959d64d8b1d727a1a4f6fae3
singhanujd <singhanujd@gmail.com>
2020-06-08 09:25:43 +0530

    rxjs maps

--- maps.ts
+++ maps.ts
@@ -4,8 +4,34 @@ import { switchMap, concatMap, mergeMap, take, exhaustMap, tap, throttleTime, de
 
 const clicks = fromEvent(document,'click');
 
-clicks
-.pipe(
-  debounceTime(5000)
-).subscribe(data => console.log(data.clientX))
+//mergeMap
+// clicks
+// .pipe(
+//   mergeMap(ev => interval(1000).pipe(take(5)))
+// ).subscribe(data => console.log(data))
 
+//switchMap
+// clicks
+// .pipe(
+//   switchMap(ev => interval(1000).pipe(take(5)))
+// ).subscribe(data => console.log(data))
+
+//concatMap
+// clicks
+// .pipe(
+//   concatMap(ev => interval(1000).pipe(take(5)))
+// ).subscribe(data => console.log(data))
+
+//throttleTime
+// Lets a value pass, then ignores source values for the next duration milliseconds.
+// clicks
+// .pipe(
+//   throttleTime(2000)
+// ).subscribe(data => console.log(data.clientX));
+
+//debounceTime
+// clicks
+// .pipe(
+//   debounceTime(2000)
+// )
+// .subscribe(data => console.log(data.clientX));
